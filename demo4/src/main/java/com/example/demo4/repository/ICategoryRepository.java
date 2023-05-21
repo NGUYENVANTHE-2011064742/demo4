b3d8ec10c8a621d1543e30bce7069920556317f4
Nguyen Van The <bilakmkiss@gmail.com>
2023-05-21 11:44:16 +0700

    Nop bai tuan 3

---
+++ demo4/src/main/java/com/example/demo4/repository/ICategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo4.repository;
+
+import com.example.demo4.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
+}
